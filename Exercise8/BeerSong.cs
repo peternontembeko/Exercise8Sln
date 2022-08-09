9d145cb34a35e9a59dd8ed3e91b27b070df01b06
peternontembeko <peternontembeko22@gmail.com>
2022-08-09 13:23:14 +0200

    Add project files.

---
+++ Exercise8/BeerSong.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    internal class Beersong
+    {
+        int _bottleofBeer;
+
+        public Beersong(int bottleofBeer)
+        {
+            _bottleofBeer = bottleofBeer;
+        }
+
+        public void Sing()
+        {
+
+            for(int i = 0; i < _bottleofBeer; i--)
+            {
+                Console.WriteLine($"{i} bottles of beer on the wall {i} bottles of beer is");
+                if (i > 2)
+                 Console.WriteLine($"Take one down and pass it around!{i - 1} bottles of beer on the wall");
+                Console.WriteLine($"Take one down and pass it around!{i - 1} bottles of beer on the wall");
+
+            }
+
+            Console.WriteLine($"1 bottle of beer on the wall! bottle of beer of beer is");
+            Console.WriteLine($"1 bottle of beer on the wall! bottle of beer of beer is");
+        }
+
+
+
+    }
+
+
+
+
+}
+    
+
+
+
+
+
+    
---
+++ Exercise8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Beersong song = new Beersong(99);
+            song.Sing();
+        }
+    }
+}
